fc5b723398cb7e5c1ce83157471fae078907ba4b
notanoty <timariefev05@gmail.com>
2024-12-17 21:53:49 +0100

    Restructuring files and adding the API

---
+++ TelegramBotAPI/src/main/java/com/notanoty/demo/GroupManagmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.notanoty.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupManagmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupManagmentApiApplication.class, args);
+	}
+
+}
---
+++ TelegramBotAPI/src/test/java/com/notanoty/demo/GroupManagmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notanoty.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupManagmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
